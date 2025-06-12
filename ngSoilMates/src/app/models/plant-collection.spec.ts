796e24535186a8d5000c4e3287fcd4cc22d28deb
Vonzink <vonzink@gmail.com>
2025-06-12 16:13:39 -0600

    complete model user.ts,user-plant.ts, species-comment.ts, plant-species.ts, plant-comments.ts, plant-collection.ts, and carelog.ts

---
+++ ngSoilMates/src/app/models/plant-collection.spec.ts
@@ -0,0 +1,7 @@
+import { PlantCollection } from './plant-collection';
+
+describe('PlantCollection', () => {
+  it('should create an instance', () => {
+    expect(new PlantCollection()).toBeTruthy();
+  });
+});
---
+++ ngSoilMates/src/app/models/species-comment.spec.ts
@@ -0,0 +1,7 @@
+import { SpeciesComment } from './species-comment';
+
+describe('SpeciesComment', () => {
+  it('should create an instance', () => {
+    expect(new SpeciesComment()).toBeTruthy();
+  });
+});
