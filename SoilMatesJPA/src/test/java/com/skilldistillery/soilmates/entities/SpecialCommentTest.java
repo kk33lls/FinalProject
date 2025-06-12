ffb0d31fd9fdd0c2f5f5e3423aaacd7c2d8c27fa
Michel <michelmukua@Ryan-s-S20.localdomain>
2025-06-12 10:57:44 -0600

    stab out species comment and care difficulty

---
+++ SoilMatesJPA/src/test/java/com/skilldistillery/soilmates/entities/SpecialCommentTest.java
@@ -0,0 +1,51 @@
+package com.skilldistillery.soilmates.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+class SpecialCommentTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private SpecialComment specialComment;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		 emf = Persistence.createEntityManagerFactory("SoilMatesJPA");
+
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+		emf = null;
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		specialComment = em.find(SpecialComment.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		em = null;
+		 
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
