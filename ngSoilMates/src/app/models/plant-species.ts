7bcae4a6eae8737cb37686639dfb6098a5df8dca
protosonic <ndebruin29@gmail.com>
2025-06-12 17:11:39 -0600

    stub out user_plant, plant_species, plant_collection ng models

--- ngSoilMates/src/app/models/plant-species.ts
+++ ngSoilMates/src/app/models/plant-species.ts
@@ -1,2 +1,52 @@
+import { SpeciesComment } from "./species-comment";
+import { UserPlant } from "./user-plant";
+
 export class PlantSpecies {
+  id: number;
+  genus: string;
+  species: string;
+  commonNames: string;
+  description: string;
+  lightRequirements: string;
+  waterFrequesncy: string;
+  nativeTo: string;
+  temperatureRange: string;
+  imageUrl: string;
+  enabled: boolean;
+  careDifficulty: CareDifficulty[];
+  userPlants: UserPlant[];
+  speciesComments: SpeciesComment[];
+
+  constructor(
+  id: number = 0,
+  genus: string = '',
+  species: string = '',
+  commonNames: string = '',
+  description: string = '',
+  lightRequirements: string = '',
+  waterFrequesncy: string = '',
+  nativeTo: string = '',
+  temperatureRange: string = '',
+  imageUrl: string = '',
+  enabled: boolean = true,
+  careDifficulty: CareDifficulty[] = [],
+  userPlants: UserPlant[] = [],
+  speciesComments: SpeciesComment[] = [],
+  ){
+  this.id = id;
+  this.genus = genus;
+  this.species = species
+  this.commonNames = commonNames;
+  this.description = description;
+  this.lightRequirements = lightRequirements;
+  this.waterFrequesncy = waterFrequesncy;
+  this.nativeTo = nativeTo;
+  this.temperatureRange = temperatureRange;
+  this.imageUrl = imageUrl;
+  this.enabled = enabled;
+  this.careDifficulty = careDifficulty;
+  this.userPlants = userPlants;
+  this.speciesComments = speciesComments;
+  }
+
 }
